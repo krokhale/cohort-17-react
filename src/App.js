351b249544a101bb02f7d9537442de71ca6b55f9
krokhale <krishna.rokhale@gmail.com>
2021-09-11 10:47:31 -0700

    checking in

--- src/App.js
+++ src/App.js
@@ -8,7 +8,22 @@ import {useState, useEffect} from 'react'
 
 function App() {
 
+  // think about problems in terms of side effects
   const [clickCounter, setClickCounter] = useState(0);
+  const [theInputTextValue, setTheInputTextValue] = useState('');
+  const [items, setItems] = useState(['a', 'b', 'c']);
+
+  // useEffect(() => {
+  //     if(clickCounter == 10){
+  //         console.log('the use effect function just ran!')
+  //         console.log('Run this code whenever the click counter changes')
+  //         alert('Yay you are the count 10')
+  //     }
+  // }, [clickCounter])
+
+    useEffect(() => {
+        console.log('this code only runs when the page loads the first time')
+    }, [])
 
   const updateCounter = async () => {
     console.log('the button was clicked!')
@@ -16,6 +31,21 @@ function App() {
     let copyOfTheCounter = clickCounter
     copyOfTheCounter = copyOfTheCounter + 1
     setClickCounter(copyOfTheCounter)
+    /*if(copyOfTheCounter == 10) {
+        alert('Yay you are the count 10')
+    }  */
+  };
+
+  const onInputChange = async (event) => {
+      setTheInputTextValue(event.currentTarget.value)
+  };
+
+  const addItem = async () => {
+      // items
+      // let copyOfTheItemsArray = items
+      // copyOfTheItemsArray.push(theInputTextValue)
+      // setItems(copyOfTheItemsArray)
+      setItems([...items, theInputTextValue])
   };
 
   return (
@@ -23,7 +53,28 @@ function App() {
         <button onClick={updateCounter}>Click me</button>
         {/*JSX*/}
 
-        <h1 style={{color: 'green'}}>Counter: {clickCounter}</h1>
+          <input onChange={onInputChange} type="text" value={theInputTextValue} />
+          <button onClick={addItem}>Add to items</button>
+
+          <p>{theInputTextValue}</p>
+
+          <ul>
+              {items.map((item) => {
+                  return <li key={item}>{item}</li>
+              })}
+          </ul>
+
+        <h1 style={{color: (clickCounter == 5 ? 'green' : 'red')}}>THE Counter: {clickCounter}</h1>
+
+          {/*logical operators */}
+          {/*ternary operators*/}
+
+          {/*OR logical operator*/}
+
+          {(clickCounter == 10 || clickCounter == 7) ?
+              <h1 style={{color: 'green'}}>The counter is now at {clickCounter}!</h1> :
+              <h1 style={{color: 'red'}}>The counter is not at {clickCounter}!</h1>}
+
 
       </div>
   );
